71842aa6d69a5acb843ed51067c13579c207c21e
Dika Bosnjak <dbosnjak@capeannenterprises.com>
2022-10-07 17:36:28 +0200

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserSearch from '../components/users/UserSearch'
+import UserResults from '../components/users/UserResults'
+
+function Home() {
+  return (
+    <div>
+        <UserSearch />
+        <UserResults />
+    </div>
+  )
+}
+
+export default Home
